dace4bdb73021ed460ee91ee2d812eb85cbbf519
tsuru7530 <yohei.tsuruta.7530@gmail.com>
2025-06-18 18:33:50 +0900

    add validation each models

--- app/models/district.rb
+++ app/models/district.rb
@@ -1,6 +1,6 @@
 class District < ApplicationRecord
     has_many :incomes, dependent: :destroy
-    validates :name, presence: true
-    validates :year, presence: true
-    validates :office, presence: true
+    validates :name, presence: true, length: {maximum: 50}
+    validates :year, presence: true, numericality:{only_integer: true, greater_than_or_equal_to: 7}
+    validates :office, presence: true, length: {maximum: 50}
 end
--- app/models/income.rb
+++ app/models/income.rb
@@ -2,7 +2,8 @@ class Income < ApplicationRecord
     belongs_to :district, optional: true
     has_many :outgoes, dependent: :destroy
     validates :district_id, presence: true
-    validates :year, presence: true
+    validates :year, presence: true, numericality:{only_integer: true, greater_than_or_equal_to: 6}
     validates :category, presence: true
-    validates :price, presence: true, numericality:{only_integer: true, greater_than_or_equal_to: 0}
+    validates :price, presence: true, numericality:{only_integer: true, greater_than_or_equal_to: 0}, length: {maximum: 10}
+    validates :memo, length: {maximum: 50}
 end
