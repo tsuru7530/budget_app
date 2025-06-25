b2517778aad01108c56be6b238f9a6adf9f6457c
tsuru7530 <yohei.tsuruta.7530@gmail.com>
2025-06-26 08:58:54 +0900

    add district model single test codes

--- spec/models/district_spec.rb
+++ spec/models/district_spec.rb
@@ -5,18 +5,26 @@ RSpec.describe District, type: :model do
     before do
       @district = FactoryBot.build(:district)
     end
-
     context "地区登録ができる" do
       it "name, year, office, imageが正しく設定されていれば登録できる" do
         expect(@district).to be_valid
       end
     end
-
     context "地区登録ができない" do
+      it "imageが空だと登録できない" do
+        @district.image = nil
+        @district.valid?
+        expect(@district.errors.full_messages).to include("Image can't be blank")
+      end
       it "nameが空だと登録できない" do
         @district.name = nil
         @district.valid?
         expect(@district.errors.full_messages).to include("Name can't be blank")
+      end
+      it "nameが51文字以上だと登録できない" do
+        @district.name = "a" * 51
+        @district.valid?
+        expect(@district.errors.full_messages).to include("Name is too long (maximum is 50 characters)")
       end
       it "yearが空だと登録できない" do
         @district.year = nil
@@ -27,6 +35,21 @@ RSpec.describe District, type: :model do
         @district.year = "hogehoge"
         @district.valid?
         expect(@district.errors.full_messages).to include("Year is not a number")
+      end
+      it "yearが7未満だと登録できない" do
+        @district.year = 6
+        @district.valid?
+        expect(@district.errors.full_messages).to include("Year must be greater than or equal to 7")
+      end
+      it "officeが空だと登録できない" do
+        @district.office = nil
+        @district.valid?
+        expect(@district.errors.full_messages).to include("Office can't be blank")
+      end
+      it "officeが51文字以上だと登録できない" do
+        @district.office = "a" * 51
+        @district.valid?
+        expect(@district.errors.full_messages).to include("Office is too long (maximum is 50 characters)")
       end
     end
   end
