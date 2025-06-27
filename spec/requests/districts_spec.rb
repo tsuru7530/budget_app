8b5818b8bfd6dd8fbb538a6426478681eadb7776
tsuru7530 <yohei.tsuruta.7530@gmail.com>
2025-06-27 09:12:59 +0900

    add districts controller test

--- spec/requests/districts_spec.rb
+++ spec/requests/districts_spec.rb
@@ -1,7 +1,77 @@
 require 'rails_helper'
 
 RSpec.describe "Districts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "#index" do
+    context "districtの一覧ページが正しく表示される(district未登録の場合)" do
+      before do
+        get districts_path
+      end
+      it "リクエストは200 OKとなる" do
+        expect(response.status).to eq 200
+      end
+      it "ページタイトルが表示されている" do
+        expect(response.body).to include("district list")
+      end
+      it '表題が表示されていない'  do
+        expect(response.body).to_not include("district name")
+        expect(response.body).to_not include("year")
+        expect(response.body).to_not include("office")
+      end
+    end
+    context "districtの一覧ページが正しく表示される(district登録済みの場合)" do
+      before do
+        @district = FactoryBot.create(:district)
+        get districts_path
+      end
+      it "リクエストは200 OKとなる" do
+        expect(response.status).to eq 200
+      end
+      it '表題が表示されている'  do
+        expect(response.body).to include("district name")
+        expect(response.body).to include("year")
+        expect(response.body).to include("office")
+      end
+      it '作成したdistrictのname, year, bodyが正しく表示されている'  do
+        expect(response.body).to include(@district.name)
+        expect(response.body).to include(@district.year)
+        expect(response.body).to include(@district.office)
+      end
+    end
+    context "districtの新規作成ページが正しく表示される" do
+      before do
+        get new_district_path
+      end
+      it "リクエストは200 OKとなる" do
+        expect(response.status).to eq 200
+      end
+    end
+    context "districtの詳細ページが正しく表示される" do
+      before do
+        @district = FactoryBot.create(:district)
+        get district_path(@district)
+      end
+      it "リクエストは200 OKとなる" do
+        expect(response.status).to eq 200
+      end
+      it "作成したdistrictのname, year, bodyが正しく表示されている" do
+        expect(response.body).to include(@district.name)
+        expect(response.body).to include(@district.year)
+        expect(response.body).to include(@district.office)
+      end
+    end
+    context "districtの更新ページが正しく表示される" do
+      before do
+        @district = FactoryBot.create(:district)
+        get edit_district_path(@district)
+      end
+      it "リクエストは200 OKとなる" do
+        expect(response.status).to eq 200
+      end
+      it "作成したdistrictのname, year, bodyが正しく表示されている" do
+        expect(response.body).to include(@district.name)
+        expect(response.body).to include(@district.year)
+        expect(response.body).to include(@district.office)
+      end
+    end
   end
 end
