b93f2848765e994632b2798fac80ae6c7c58748a
tsuru7530 <yohei.tsuruta.7530@gmail.com>
2025-07-01 17:57:12 +0900

    modify district model single test

--- spec/models/district_spec.rb
+++ spec/models/district_spec.rb
@@ -6,49 +6,49 @@ RSpec.describe District, type: :model do
       @district = FactoryBot.build(:district)
     end
     context "地区登録ができる" do
-      it "name, year, office, imageが正しく設定されていれば登録できる" do
+      it "地区名, 年度, 事務所名, 画像が正しく設定されていれば登録できる" do
         expect(@district).to be_valid
       end
-      it "imageが空でも登録できる" do
+      it "画像が空でも登録できる" do
         @district.image = nil
         expect(@district).to be_valid
       end
     end
     context "地区登録ができない" do
-      it "nameが空だと登録できない" do
+      it "地区名が空だと登録できない" do
         @district.name = nil
         @district.valid?
-        expect(@district.errors.full_messages).to include("Name can't be blank")
+        expect(@district.errors.full_messages).to include("地区名を入力してください")
       end
-      it "nameが51文字以上だと登録できない" do
+      it "地区名が51文字以上だと登録できない" do
         @district.name = "a" * 51
         @district.valid?
-        expect(@district.errors.full_messages).to include("Name is too long (maximum is 50 characters)")
+        expect(@district.errors.full_messages).to include("地区名は50文字以内で入力してください")
       end
-      it "yearが空だと登録できない" do
+      it "年度が空だと登録できない" do
         @district.year = nil
         @district.valid?
-        expect(@district.errors.full_messages).to include("Year can't be blank")
+        expect(@district.errors.full_messages).to include("年度を入力してください")
       end
-      it "yearが数字以外だと登録できない" do
+      it "年度が数字以外だと登録できない" do
         @district.year = "hogehoge"
         @district.valid?
-        expect(@district.errors.full_messages).to include("Year is not a number")
+        expect(@district.errors.full_messages).to include("年度は数値で入力してください")
       end
-      it "yearが7未満だと登録できない" do
+      it "年度が7未満だと登録できない" do
         @district.year = 6
         @district.valid?
-        expect(@district.errors.full_messages).to include("Year must be greater than or equal to 7")
+        expect(@district.errors.full_messages).to include("年度は7以上の値にしてください")
       end
-      it "officeが空だと登録できない" do
+      it "事務所名が空だと登録できない" do
         @district.office = nil
         @district.valid?
-        expect(@district.errors.full_messages).to include("Office can't be blank")
+        expect(@district.errors.full_messages).to include("事務所名を入力してください")
       end
-      it "officeが51文字以上だと登録できない" do
+      it "事務所名が51文字以上だと登録できない" do
         @district.office = "a" * 51
         @district.valid?
-        expect(@district.errors.full_messages).to include("Office is too long (maximum is 50 characters)")
+        expect(@district.errors.full_messages).to include("事務所名は50文字以内で入力してください")
       end
     end
   end
