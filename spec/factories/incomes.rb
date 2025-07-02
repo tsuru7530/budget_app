68bdb8d7acd2c0033bcef21c8b96b52c47cc8ac6
tsuru7530 <yohei.tsuruta.7530@gmail.com>
2025-07-02 18:56:10 +0900

    modify pulldown

--- spec/factories/incomes.rb
+++ spec/factories/incomes.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :income do
     year {Faker::Number.number(digits: 2)}
-    category {"現年"}
+    category {"当初"}
     price {Faker::Number.number(digits: 8)}
     memo {Faker::Lorem.characters(number: 20)}
   end
