61da92829a6c8d7add0929e088c78c3f850ce636
tsuru7530 <yohei.tsuruta.7530@gmail.com>
2025-07-13 14:21:25 +0900

    add address search function

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -63,6 +63,6 @@ class DistrictsController < ApplicationController
 
     private
     def district_params
-        params.require(:district).permit(:name, :year, :office, :image, :image_delete, :latitude, :longitude)
+        params.require(:district).permit(:name, :year, :office, :image, :image_delete, :latitude, :longitude, :address)
     end
 end
--- db/migrate/20250709035059_add_column_districts.rb
+++ db/migrate/20250709035059_add_column_districts.rb
@@ -3,5 +3,6 @@ class AddColumnDistricts < ActiveRecord::Migration[8.0]
       add_column :districts, :latitude, :float
       add_column :districts, :longitude, :float
       add_column :districts, :image_delete, :integer
+      add_column :districts, :address, :string
   end
 end
